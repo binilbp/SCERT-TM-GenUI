af25625f4b45f81931f08e98c29992100921d05e
binilbp <biniloffcl@gmail.com>
2025-12-29 20:44:00 +0530

    Change the data file for datafile with new json form

--- src/lib/assets/data.ts
+++ src/lib/assets/data.ts
@@ -1,6 +1,8 @@
 export interface Chapter {
 	chapterNumber: string;
 	title: string;
+	minPage: string;
+	maxPage: string;
 }
 
 interface Subject {
@@ -22,115 +24,30 @@ export const classData: ClassDataType = [
 			{
 				name: 'Malalyalam',
 				chapters: [
-					{ chapterNumber: '1', title: 'മഴയോ മഴ' },
-					{ chapterNumber: '2', title: 'നന്നായ് ഉണ്ണാൻ' },
-					{ chapterNumber: '3', title: 'ഏന്താണമ്മേ സ്വാതന്ത്ര്യം' },
-					{ chapterNumber: '4', title: 'പൂരം പൊടിപൂരം' },
-					{ chapterNumber: '5', title: 'രുചിമേളം' }
+					{ chapterNumber: '6', title: 'പറക്കും വീട് ', minPage: '62', maxPage: '78' },
+					{ chapterNumber: '7', title: 'മണിയൻ്റെ യാത്ര', minPage: '88', maxPage: '99' },
+					{ chapterNumber: '8', title: 'പച്ചിലത്തുമ്പിലൂടെ', minPage: '100', maxPage: '109' },
+					{ chapterNumber: '9', title: 'ആരോഗ്യവും സമ്പത്ത്', minPage: '110', maxPage: '121' }
 				]
 			},
 			{
 				name: 'English',
 				chapters: [
-					{ chapterNumber: '1', title: 'An Umbrell For All' },
-					{ chapterNumber: '2', title: 'A Piece Of Cake' },
-					{ chapterNumber: '3', title: 'The Village Of Birds' }
+					{ chapterNumber: '3', title: 'The Village Of Birds', minPage: '63', maxPage: '102' },
+					{ chapterNumber: '4', title: 'Star In The Jar', minPage: '111', maxPage: '142' }
 				]
 			},
 			{
 				name: 'Maths',
 				chapters: [
-					{ chapterNumber: '1', title: 'അടുക്കളയിലെ രൂപങ്ങൾ' },
-					{ chapterNumber: '2', title: 'ഒളിച്ചേ... കണ്ടേ' },
-					{ chapterNumber: '3', title: 'നല്ല നാടിനായ്... നല്ല നാളേക്കായ്' },
-					{ chapterNumber: '4', title: 'സമ്മാനപ്പൊതി' },
-					{ chapterNumber: '5', title: 'ഒന്നായ് ചേരാം നന്നായ് വളരാം' }
-				]
-			},
-			{
-				name: 'Arab',
-				chapters: [
-					{ chapterNumber: '1', title: 'الوَحْدَةُ الأُولَى: الأَرْضُ الجَمِيلةُ' },
-					{ chapterNumber: '2', title: 'الْبِينَةُ النَّظِيفَةُ' },
-					{ chapterNumber: '3', title: 'الْوَحْدَةُ الثَّالِثَةُ - الْوَطَنُ الْحَبِيبُ' },
-					{ chapterNumber: '4', title: 'الطَّيْرُ الْحَنُونُ' },
-					{ chapterNumber: '5', title: 'الْعَالَمُ الْعَجِيبُ' },
-					{ chapterNumber: '6', title: 'القرية السعيدة' }
-				]
-			}
-		]
-	},
-	{
-		classNumber: '3',
-		subjects: [
-			{
-				name: 'Malayalam',
-				chapters: [
-					{ chapterNumber: '1', title: 'വാമൊഴിച്ചന്തം' },
-					{ chapterNumber: '2', title: 'പലഹാരപ്പൊതി' },
-					{ chapterNumber: '3', title: 'മനുഷ്യന്റെ കൈകൾ' }
-				]
-			},
-			{
-				name: 'English',
-				chapters: [
-					{ chapterNumber: '1', title: 'Tender Touch' },
-					{ chapterNumber: '2', title: 'Festive Fun' },
-					{ chapterNumber: '3', title: 'In Harmony' },
-					{ chapterNumber: '4', title: 'Blooms and Breeze' },
-					{ chapterNumber: '5', title: 'Honesty Pays' }
-				]
-			},
-			{
-				name: 'Maths',
-				chapters: [
-					{ chapterNumber: '1', title: 'രൂപങ്ങൾ വരയ്ക്കാം' },
-					{ chapterNumber: '2', title: 'സംഖ്യകൾ കൂട്ടുകാർടേ' },
-					{ chapterNumber: '3', title: 'ആയിരം... ആയിരം' },
-					{ chapterNumber: '4', title: 'കൂട്ടുകൂടാം' }
-				]
-			},
-			{
-				name: 'EVS',
-				chapters: [
-					{ chapterNumber: '1', title: 'ഹരിതഭൂമി' },
-					{ chapterNumber: '2', title: 'ജന്തുലോകത്തേക്ക്' },
-					{ chapterNumber: '3', title: 'ജലവും മണ്ണും' },
-					{ chapterNumber: '4', title: 'വൃത്തി നൽകും ശക്തി' }
-				]
-			}
-		]
-	},
-	{
-		classNumber: '4',
-		subjects: [
-			{
-				name: 'Malayalam',
-				chapters: [
-					{ chapterNumber: '1', title: 'ചിറകുവിരിച്ച്' },
-					{ chapterNumber: '2', title: 'ഇനിയും മുന്നോട്ട്' },
-					{ chapterNumber: '3', title: 'കാറ്റേ കടലേ' }
-				]
-			},
-			{
-				name: 'English',
-				chapters: [
-					{ chapterNumber: '1', title: 'Beyond Fear' },
-					{ chapterNumber: '2', title: 'Voices in Unison' },
-					{ chapterNumber: '3', title: 'Pages of Wonder' }
-				]
-			},
-			{
-				name: 'Maths',
-				chapters: [{ chapterNumber: '1', title: 'വരകൾ രൂപങ്ങൾ' }]
-			},
-			{
-				name: 'EVS',
-				chapters: [
-					{ chapterNumber: '1', title: 'ജീവികളും ചുറ്റുപാടും' },
-					{ chapterNumber: '2', title: 'ഇന്ത്യ എന്റെ രാജ്യം' },
-					{ chapterNumber: '3', title: 'ചിറകുള്ള കൂട്ടുകാർ' },
-					{ chapterNumber: '4', title: 'പരിസരപഠനം — കലാകേരളം' }
+					{
+						chapterNumber: '5',
+						title: 'ഒന്നായ് ചേരാം നന്നായ് വളരാം',
+						minPage: '68',
+						maxPage: '86'
+					},
+					{ chapterNumber: '6', title: 'കാട്ടിലെ ഉത്സവം', minPage: '96', maxPage: '104' },
+					{ chapterNumber: '7', title: 'ചങ്ങാതിക്കൂട്ടം', minPage: '112', maxPage: '129' }
 				]
 			}
 		]
